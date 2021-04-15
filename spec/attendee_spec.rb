c3122f078bc895287cc233be48ea7b7ed96290d6
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-15 09:12:16 -0600

    Finalize initialize test set

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -11,7 +11,12 @@ RSpec.describe 'Attendee' do
       attendee = Attendee.new('Bob', 20)
 
       expect(attendee.name).to eq('Bob')
-      expect(attendee.age).to eq(20)
+      expect(attendee.spending_money).to eq(20)
+    end
+    it 'starts with no interests' do
+      attendee = Attendee.new('Bob', 20)
+
+      expect(attendee.interests).to eq([])
     end
   end
 end
