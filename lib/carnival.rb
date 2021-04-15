3fc9e6eeba883c56a910773be3cbfd232df7fdca
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-15 09:38:39 -0600

    Green add_ride Red recomend_rides

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,9 +1,13 @@
 class Carnival
   attr_reader :name,
               :rides
-              
+
   def initialize(name)
     @name = name
     @rides = []
+  end
+
+  def add_ride(ride)
+    @rides.push(ride)
   end
 end
