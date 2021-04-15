50591bf0ddfb20b131bc3258e7f4b53157530563
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-15 09:18:24 -0600

    Ride initialize test

---
+++ lib/ride.rb
@@ -0,0 +1,4 @@
+class Ride
+  def initialize(attributes)
+  end
+end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe 'Ride' do
 
       expect(ride.class).to eq(Ride)
     end
+    it 'has a name and a cost' do
+      ride = Ride.new({name: 'Ferris Wheel', cost: 0})
+
+      expect(ride.name).to eq('Ferris Wheel')
+      expect(ride.cost).to eq(0)
+    end
   end
 end
