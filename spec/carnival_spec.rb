298702ca942c3c72dc28dd07c92cf9ba167221b0
Lawrence Whalen <ldwhalen@gmail.com>
2021-04-15 09:29:50 -0600

    Green creates carnival test Red intialize variables test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,6 +1,6 @@
 require './lib/carnival'
-require '/lib/ride'
-require '/lib/attendee'
+require './lib/ride'
+require './lib/attendee'
 
 RSpec.describe 'Carnival' do
   describe '#initialize' do
@@ -8,6 +8,12 @@ RSpec.describe 'Carnival' do
       jeffco_fair = Carnival.new("Jefferson County Fair")
 
       expect(jeffco_fair.class).to eq(Carnival)
+    end
+    it 'has a name and an empyt array for rides' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+
+      expect(jeffco_fair.name).to eq('Jefferson County Fair')
+      expect(jeffco_fair.ride).to eq([])
     end
   end
 end
